0cbfda4b92bd99087aa27f26329ee9b033928b18
Mark Kevin Mayormita <michaelortizmayormita@Michaels-MacBook-Air.local>
2026-06-04 14:37:16 +0800

    fix expense history table orderby

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -24,8 +24,8 @@ end
 
 puts "Created #{created_categories.count} categories"
 
-# Generate expenses from January 2024 to February 18, 2026
-puts "Creating expenses from January 2024 to February 18, 2026..."
+# Generate expenses from January 2024 to June 18, 2026
+puts "Creating expenses from January 2024 to June 18, 2026..."
 
 # Define expense templates for variety
 expense_templates = {
@@ -112,9 +112,9 @@ expense_templates = {
 }
 
 # Start date: January 1, 2024
-# End date: February 18, 2026
+# End date: June 18, 2026
 start_date = Date.new(2024, 1, 1)
-end_date = Date.new(2026, 2, 18)
+end_date = Date.new(2026, 6, 18)
 
 expense_count = 0
 current_date = start_date
